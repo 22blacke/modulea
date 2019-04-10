14b85e7e3bd48bb7f844941480c6bf22736ffd78
22blacke <49483181+22blacke@users.noreply.github.com>
2019-04-10 12:43:43 -0400

    adding hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,13 @@
+#keary
+#04/10/2019
+
+#################
+#hello world    #
+#               #
+#               #
+# first  python #
+#               #
+#################
+
+#print "hello, world" to the screen.
+print("Hello,World")
